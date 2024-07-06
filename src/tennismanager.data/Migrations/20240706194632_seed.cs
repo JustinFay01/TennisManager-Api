6d176ed4b1e7dbe012aa3e99e633bfb044d2f04e
Justin Fay <justin.fay@hope.edu>
2024-07-06 15:47:02 -0400

    Rebuilt db
    Signed-off-by: Justin Fay <justin.fay@hope.edu>

---
+++ src/tennismanager.data/Migrations/20240706194632_seed.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using tennismanager.shared;
+
+#nullable disable
+
+namespace tennismanager.data.Migrations
+{
+    /// <inheritdoc />
+    public partial class seed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Make sure no errors occur if the data already exists
+            migrationBuilder.Sql(
+                $@"DELETE FROM public.""Users"" where ""Users"".""Id"" = '{SystemUserIds.JustinFayId}'");
+            
+            migrationBuilder.Sql(
+                $@"INSERT INTO public.""Users"" (""Id"", ""FirstName"", ""LastName"", ""UserType"") Values ('{SystemUserIds.JustinFayId}', 'Justin', 'Fay', 'Coach')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                $@"DELETE FROM public.""Users"" where ""Users"".""Id"" = '{SystemUserIds.JustinFayId}'");
+        }
+    }
+}
