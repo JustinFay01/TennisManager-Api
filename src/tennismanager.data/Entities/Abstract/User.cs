be55f3c3ccedbd7b4458e497763c429521d624eb
Justin Fay <justinfay501@gmail.com>
2024-09-28 21:15:54 -0400

    Auth0 User Fields

--- src/tennismanager.data/Entities/Abstract/User.cs
+++ src/tennismanager.data/Entities/Abstract/User.cs
@@ -6,6 +6,13 @@ public abstract class User : BaseEntity
     public string LastName { get; set; }
     public string? Email { get; set; }
     public string? PhoneNumber { get; set; }
+    
+    // Auth0 Fields
+    public string? Nickname { get; set; }
+    public string? Picture { get; set; }
+    public string? Sub { get; set; }
+    public string? Auth0Provider => !string.IsNullOrEmpty(Sub) ? Sub.Split("|")[0] : null;
+    public string? Auth0Id => !string.IsNullOrEmpty(Sub) ? Sub.Split("|")[1] : null;
 }
 
 public class Admin : User
---
+++ src/tennismanager.data/Migrations/20240929011459_auth0Fields.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace tennismanager.data.Migrations
+{
+    /// <inheritdoc />
+    public partial class auth0Fields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Nickname",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Picture",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Sub",
+                table: "Users",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Nickname",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Picture",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Sub",
+                table: "Users");
+        }
+    }
+}
